9759c6730a1e67ced792ee143f7d8b8f32ca66a9
utkarsh6080 <97819976+utkarsh6080@users.noreply.github.com>
2025-04-14 00:31:23 -0700

    Utkarsh's update

--- godb/buffer_pool_extra.go
+++ godb/buffer_pool_extra.go
@@ -6,15 +6,41 @@ import (
 
 // Rolls back a transaction by reading the log and undoing the changes made by
 // the transaction.
+// Rollback undoes all changes made by the transaction identified by tid.
 func (bp *BufferPool) Rollback(tid TransactionID) error {
-	// TODO: some code goes here
-	return fmt.Errorf("not implemented") // replace it
+	iter, err := bp.logFile.ReverseIterator()
+	if err != nil {
+		return fmt.Errorf("failed to create reverse iterator: %w", err)
+	}
+	for {
+		record, err := iter()
+		if err != nil {
+			return fmt.Errorf("error reading log record: %w", err)
+		}
+		if record == nil {
+			break
+		}
+		if record.Tid() != tid {
+			continue
+		}
+
+		if updateRec, ok := record.(*UpdateLogRecord); ok {
+			beforePage := updateRec.Before
+
+			if err := beforePage.getFile().flushPage(beforePage); err != nil {
+				return fmt.Errorf("failed to flush page during rollback: %w", err)
+			}
+			beforePage.setDirty(-1, false)
+		}
+	}
+
+	return nil
 }
 
 // Returns the log file associated with the buffer pool.
 func (bp *BufferPool) LogFile() *LogFile {
 	// TODO: some code goes here
-	return nil // replace it
+	return bp.logFile
 }
 
 // Recover the buffer pool from a log file. This should be called when the
--- godb/heap_page_extra.go
+++ godb/heap_page_extra.go
@@ -3,15 +3,39 @@ package godb
 // Returns the before-image of the page. This is used for logging and recovery.
 func (p *heapPage) BeforeImage() Page {
 	// TODO: some code goes here
+	if p.beforeImage != nil {
+		return p.beforeImage
+	}
+	// Create a copy manually.
+	newPg := *p
+	// Clear the beforeImage of the copy
+	newPg.beforeImage = nil
+	// Copy the tuples slice so that changes to the original won't affect the copy.
+	if p.tuples != nil {
+		newPg.tuples = make([]*Tuple, len(p.tuples))
+		copy(newPg.tuples, p.tuples)
+	}
+	return &newPg
 }
 
 // Sets the before-image of the page to the current state of the page. Be sure
 // that changing the page does not change the before-image.
 func (p *heapPage) SetBeforeImage() {
 	// TODO: some code goes here
+	// Create a copy of the current page.
+	newPg := *p
+	// Set the beforeImage field of the copy to nil
+	newPg.beforeImage = nil
+	// Copy the tuples slice.
+	if p.tuples != nil {
+		newPg.tuples = make([]*Tuple, len(p.tuples))
+		copy(newPg.tuples, p.tuples)
+	}
+	p.beforeImage = &newPg
 }
 
 // Returns the page number of the page.
 func (p *heapPage) PageNo() int {
 	// TODO: some code goes here
+	return p.pageNo
 }
